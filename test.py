42086c6d593cc875e1c6a6e211f5338c6f1a24e8
Mariya Khan <mariya.k2022@gmail.com>
2025-04-24 02:27:58 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,18 @@
-if test != 1:
+class Test:
+    def thisisatest(me, you):
+        print(f"called with {you}")
+
+test = Test()
+
+test_val = 1
+
+if test_val != 1:
     test.thisisatest(False)
-elif test == 1:
+elif test_val == 1:
     test.thisisatest(True)
-else:
-    quit()
 
 x = 5
 y = 4
 
-if (test > 0): 
-    print(x*y)
+if test_val > 0:
+    print(x * y)
